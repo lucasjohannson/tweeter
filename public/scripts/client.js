9600dd84dcd0b88731d27393ede07ae9cd7167d1
lucas <lucas.johannson1@ucalgary.ca>
2021-06-16 22:31:46 +0000

    posts are now fully dynamic and will be updated in real time

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,30 +6,30 @@
 
 $(document).ready(function(){
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+//   const data = [
+//     {
+//       "user": {
+//         "name": "Newton",
+//         "avatars": "https://i.imgur.com/73hZDYK.png"
+//         ,
+//         "handle": "@SirIsaac"
+//       },
+//       "content": {
+//         "text": "If I have seen further it is by standing on the shoulders of giants"
+//       },
+//       "created_at": 1461116232227
+//     },
+//     {
+//       "user": {
+//         "name": "Descartes",
+//         "avatars": "https://i.imgur.com/nlhLi3I.png",
+//         "handle": "@rd" },
+//       "content": {
+//         "text": "Je pense , donc je suis"
+//       },
+//       "created_at": 1461113959088
+//     }
+//   ]
 
   const renderTweets = function(tweets) {
     // loops through tweets
@@ -41,8 +41,17 @@ $(document).ready(function(){
   }
 
   const loadTweets = function () {
-    
+    $.ajax({
+      url: "http://localhost:8080/tweets",
+      method: 'GET',
+      success: function(res) {
+        renderTweets(res);
+      }
+    });
   }
+
+  loadTweets();
+
 
   const createTweetElement = function (obj) { return $(
     `
@@ -79,7 +88,6 @@ $(document).ready(function(){
   `
   )}
 
-
   $( ".target" ).submit(function( event ) {
     event.preventDefault();
     //const target = event.currentTarget;
@@ -90,12 +98,13 @@ $(document).ready(function(){
       url: "/tweets",
       method: "POST",
       data: $(this).serialize(),
-    });
+    })
+    .then (() => {
+      this.reset();
+      loadTweets();
+    })
 
   });
-
-  renderTweets(data);
-
 
 });
 
