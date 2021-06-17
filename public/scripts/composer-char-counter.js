0a931a9e29d15697dc9e7f02a2b64a4fa51a87c9
lucas <lucas.johannson1@ucalgary.ca>
2021-06-17 22:50:03 +0000

    general clean up and commenting

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,30 +1,21 @@
 $(document).ready(function() {
   // --- our code goes here ---
-  const inputField = document.getElementById("tweet-text");
-  const time = document.getElementById("time");
-  //time.innerHTML = timeago.format(new Date());
-  // console.log(timeago.format(new Date()));
-  const escape = function (str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
 
+  const inputField = document.getElementById("tweet-text");
+  
+  //event handler to display the character cound 
   inputField.addEventListener('input', function(event) {
+    //initialize vars to use 
     const target = event.currentTarget;
     const maxLength = target.getAttribute("max");
     const currentLength = target.value.length;
-
+    //if the current length of tweet greater than the max then display red else display grey font
     if (currentLength >= maxLength) {
       $('.counter').css('color','red');
     } else {
       $('.counter').css('color','dimgray');
     }
-
-    // console.log(`${maxLength - currentLength} chars left`);
-    // console.log(this); 
-    // console.log(event);
-
+    //update the html to display the available chars remaining.
     this.parentElement.parentElement.lastElementChild.lastElementChild.innerHTML = maxLength - currentLength;
   });
 
